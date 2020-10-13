05bd1febe7588d75e25129adde4878691616a3f8
Yagnesh Limbachiya <yagneshlimbachiya7@gmail.com>
2020-10-13 23:26:10 +0530

    Header setup

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className = "header">
+            {/* logo on the lesft side */}
+            {/* search bar */}
+            {/* 3 links */}
+            {/* Basket icon with number */}
+        </nav>
+    )
+}
+
+export default Header
+
+
+{/* rfce */}
